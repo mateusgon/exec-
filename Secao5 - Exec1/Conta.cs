15bb059bd5bd73217cca6bcdaca1d620f74e5ca8
Mateus Gonçalo <mateusgon57@gmail.com>
2019-01-04 16:45:55 -0200

    Iniciando exercicio proposto

---
+++ Secao5 - Exec1/Conta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Secao5___Exec1
+{
+    class Conta
+    {
+        public int NumConta { get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int NumConta, string Titular)
+        {
+            this.NumConta = NumConta;
+            this.Titular = Titular;
+        }
+
+        public Conta(int NumConta, string Titular, double Saldo) : this(NumConta, Titular)
+        {
+            this.Saldo = Saldo;
+        }
+
+        public void Sacar(double Valor)
+        {
+            this.Saldo = this.Saldo - Valor;
+        }
+
+        public void Depositar(double Valor)
+        {
+            this.Saldo = this.Saldo + Valor;
+        }
+        
+        public override string ToString()
+        {
+            return "Conta: " + NumConta + ", Titular: " + Titular + ", Saldo: " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Secao5 - Exec1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Secao5___Exec1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+            Console.Write("Digite o número da conta: ");
+            int numConta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Deseja realizar um depósito inicial?");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Digite o valor do depósito: ");
+                double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numConta, nome, valor);
+            }
+            else
+            {
+                conta = new Conta(numConta, nome);
+            }
+            Console.WriteLine(conta.ToString());
+        }
+    }
+}
